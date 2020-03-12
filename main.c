1b650565de5fff3e88129ffdc4fc806c90860268
Tom Herman <tomhe@campus.technion.ac.il>
2020-03-12 16:01:48 +0200

    few more fixes and documentation

--- main.c
+++ main.c
@@ -3,17 +3,39 @@
 #include <stdlib.h>
 
 
+/*
+ * Graphs in this project are represented as list of int* (node),
+ *  each node points to an int-array (the node's neighbours).
+ *   In the first index there is the number of neighbours, and after it the neighbours themselves (a node is represented as an int).
+ */
+
 //typedef struct _list {
 //    int value;
 //    void* next;
 //} * List;
 
-int** createPolyominoGraph(int p) {
-    int originCell = p-2;
-    int height = 2*p-2;
+/*  _ _ _ _
+ * |_|_|_|_|
+ * |_|_|_|_|
+ * |_|_|_|_|
+ * |S|_|_|_|
+ *   |_|_|_|
+ *   |_|_|_|
+ *
+ *  Example for p=4 graph. originCell is 'S'.
+*/
+/// \param p - max number of movement in the graph, from the origin cell.
+/// \param originCellPtr -  output ptr for the origin cell of the graph.
+/// \return - the created graph.
+int** createPolyominoGraph(int p, int* originCellPtr) {
+    int originCell = p>=2 ? p-2 : 0;
+    *originCellPtr = originCell;
+    int height = p + originCell;
     int n = p*height;
+
     int** nodes = malloc(n * sizeof(int*));
     int* neighboursGlobal = malloc(n*5* sizeof(int));
+
     for(int i = originCell; i < n; i++) {
         int* neighbours = neighboursGlobal + 5*i;
         int counter = 0;
@@ -31,21 +53,26 @@ int** createPolyominoGraph(int p) {
     return nodes;
 }
 
-int recCounter(int** nodes, int p, ...) {
-    return 0;
-}
-
-void deleteGraph(int** nodes, int p) {
-    int originCell = p-2;
+void deleteGraph(int** nodes, int p, int originCell) {
     int* neighboursGlobal = nodes[originCell] - 5*originCell;
     free(nodes);
     free(neighboursGlobal);
 }
 
+int recCounter(int** nodes, int p, int originCell/*, ...*/) {
+    return 0;
+}
+
+int countSubGraphs(int** nodes, int p, int originCell) {
+    return recCounter(nodes, p, originCell/*, ...*/);
+}
+
 int countPolyominoes(int p) {
-    int** g = createPolyominoGraph(p);
-    int count = deleteGraph(g, p, ...);
-    deleteGraph(g, p);
+    if (p < 1) return 0;
+    int originCell;
+    int** nodes = createPolyominoGraph(p, &originCell);
+    int count = countSubGraphs(nodes, p, originCell);
+    deleteGraph(nodes, p, originCell);
     return count;
 }
 
